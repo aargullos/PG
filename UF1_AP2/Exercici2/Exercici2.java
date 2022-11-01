47d885814dc056a9f1ae910a90ef5bd57e9093b3
arnold <95426496+aargullos@users.noreply.github.com>
2022-11-01 16:25:53 +0100

    Create Exercici2.java

---
+++ UF1_AP2/Exercici2/Exercici2.java
@@ -0,0 +1,18 @@
+//a Arnau Argullós 
+
+/* b
+Nom: Arnau
+Cognoms: Argullós
+INS Manuel Vázquez Montalbán
+Data d’edició: 28/10/2022 - 11:53
+Nom del cicle formatiu: 2n DAW
+Nom del mòdul: M03 
+ */
+
+package cat.institutmvm;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
