35794545e534b67c79574aa76698133380b9ad37
Arnxld <95426496+aargullos@users.noreply.github.com>
2022-11-02 14:01:39 +0100

    Create Exercici7d.java

---
+++ UF1_AP2/Exercici7/Exercici7d.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Exercici7 {
+    public static void main(String[] args) {
+        int count=100;
+        Scanner sc = new Scanner(System.in);
+        while (count >= 0) {
+            System.out.println(count);
+            count = count-1;
+        }
+    }
+}
